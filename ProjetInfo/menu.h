9ab1efc635cfd6aea5d9dda2757fea54531aee50
Alexis Degrange <alexis.degrange1@gmail.com>
2020-04-30 22:45:09 +0200

    Menuv1

---
+++ ProjetInfo/menu.h
@@ -0,0 +1,57 @@
+#ifndef _MENU
+#define _MENU
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+class OptionMenu
+{
+    string nom_;
+    string description_;
+  public:
+    OptionMenu(const string &nom,const string &description);
+    string getNom() const {return nom_;}
+    string getDescription() const {return description_;}
+};
+
+
+class Menu
+{
+    string titre_;
+    vector<OptionMenu> listeOptions_;
+  public:
+    Menu(const string & titre);
+    void setTitre(const string &titre);
+    void ajouterOption(const string &nom,const string &description);
+    void afficherMenu();
+    int demanderChoix();
+    void executer();
+    virtual void executerOption(const string &nom,bool &fin);
+    bool quitter();
+    virtual void aide();
+};
+
+class MenuPoker : public Menu
+{
+
+public:
+    MenuPoker();
+
+};
+
+
+/*class MenuMoyenne : public Menu
+{
+    Semestre* semestre_;
+public:
+    MenuMoyenne(Semestre* semestre);
+    void executerOption(const string& nom, bool& fin);
+    void saisirMatieres();
+    void saisirNotes();
+    void calculerMoyenne();
+};*/
+#endif
+
